44c7f28de9b5dba758fb12fe0f71980a02fbc4c5
Zakaria Kerkeb <hikkary@mac.local>
2024-03-05 09:57:42 +0100

    to Localhost

--- src/App.js
+++ src/App.js
@@ -1,111 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary hooks and styled-components for styling
+import { useState } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import './App.css'; // Importing CSS for global styles
 
+
+const controller = new AbortController();
+
+// The main functional component for the app
 function App() {
+  // State hooks for managing user input, conversation history, loading status, and the current answer
   const [input, setInput] = useState('');
   const [conversations, setConversations] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [currentAnswer, setCurrentAnswer] = useState('');
 
-  // const handleSubmit = () => {
-  //   const newConversations = [...conversations, { type: 'question', text: input }];
-  //   setConversations(newConversations);
-  //   setIsLoading(true);
-   
-  //   fetch('http://ai.kerkeb.com/api/generate', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       "model": "mistral:instruct",
-  //       "prompt": input,
-  //       "stream": true, // Activer le mode flux
-  //     }),
-  //   })
-  //   .then(response => {
-  //     const reader = response.body.getReader();
-  //     let decoder = new TextDecoder();
-  //     let data = '';
-  
-  //     function processText({ done, value }) {
-  //       if (done) {
 
-  //         setConversations(convs => [...convs, { type: 'answer', text: currentAnswer}]);
-  //         setCurrentAnswer('')
-  //         setIsLoading(false);
-  //         return;
-  //       }
-  //       data += decoder.decode(value, { stream: true });
-  //       try {
-  //         let lastNewlineIndex = data.lastIndexOf('\n');
-  //         if (lastNewlineIndex !== -1) {
-  //           let messages = data.slice(0, lastNewlineIndex).split('\n');
-  //           messages.forEach(message => {
-  //             if (message.trim()) {
-  //               console.log('message', message);
-  //               let json = JSON.parse(message);
-  //               if (json.response) {
-  //                 // textResponse += json.response;
-  //                 setCurrentAnswer(currentAnswer + json.response);
-  //                 // setConversations(convs => [...convs, { type: 'answer', text: json.response }]);
-  //               }
-  //             }
-  //           });
-  //           data = data.slice(lastNewlineIndex + 1);
-  //         }
-  //       } catch (error) {
-  //         console.error('Error processing JSON', error);
-  //       }
-  //       reader.read().then(processText).catch(error => {
-  //         console.log(error);
-  //         setIsLoading(false);
-  //       });
-  //     }
-  
-  //     reader.read().then(processText).catch(error => {
-  //       console.log(error);
-  //       setIsLoading(false);
-  //     });
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //     setIsLoading(false);
-  //   });
-  // }
+
+  // Function to handle the submit action
   const handleSubmit = () => {
-    // Ajouter la question à la conversation
+    // Add the current question to the conversations list
     setConversations(convs => [...convs, { type: 'question', text: input }]);
+    // Set loading state to true
     setIsLoading(true);
-    setCurrentAnswer(''); // Réinitialiser la réponse courante
-  
-    fetch('http://ai.kerkeb.com/api/generate', {
-      // fetch('http://localhost:11434/api/generate', {
+    // Reset the current answer
+    setCurrentAnswer('');
+
+    // Fetch data from the AI API
+    // fetch('http://ai.kerkeb.com/api/generate', {
+      fetch('http://127.0.0.1:8070/api/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
+      signal: controller.signal,
+      // Sending the user input and additional parameters to the API
       body: JSON.stringify({
         "model": "mistral:instruct",
         "prompt": input,
-        "system": "talk to me like a muslim",
-        "stream": true, // Activer le mode flux
+        "system": "talk to me like a react teacher, use functional components and ES6 syntax, and hooks",
+        "stream": true, // Enable streaming mode
       }),
     })
-    .then(response => {
+    .then(response => { 
       const reader = response.body.getReader();
       let decoder = new TextDecoder();
       let data = '';
-      let tempResponse = ''; // Accumuler la réponse ici
-  
+      let tempResponse = ''; // Temporary storage for the response
+
+      // Function to process the text stream
       function processText({ done, value }) {
         if (done) {
-          // Ajouter la réponse complète à la conversation
-          setCurrentAnswer(''); // Réinitialiser la réponse courante
-          
+          // When done, update the conversation with the full response
+          setCurrentAnswer(''); // Reset the current answer
           setConversations(convs => [...convs, { type: 'answer', text: tempResponse }]);
           setIsLoading(false);
           return;
@@ -116,11 +62,12 @@ function App() {
           if (lastNewlineIndex !== -1) {
             let messages = data.slice(0, lastNewlineIndex).split('\n');
             messages.forEach(message => {
+               console.log(message)
               if (message.trim()) {
                 let json = JSON.parse(message);
                 if (json.response) {
-                  tempResponse += json.response; // Accumuler la réponse
-                  setCurrentAnswer(tempResponse); // Mettre à jour la réponse courante
+                  tempResponse += json.response; // Accumulate the response
+                  setCurrentAnswer(tempResponse); // Update the current answer with accumulated response
                 }
               }
             });
@@ -134,7 +81,7 @@ function App() {
           setIsLoading(false);
         });
       }
-  
+
       reader.read().then(processText).catch(error => {
         console.log(error);
         setIsLoading(false);
@@ -146,30 +93,36 @@ function App() {
     });
   }
   
+  // Render method for the app
   return (
     <Container>
       <ConversationContainer>
+        {/* Map through conversations and display them */}
         {conversations.map((conv, index) => (
           <Message key={index} isQuestion={conv.type === 'question'}>
             {conv.text}
           </Message>
         ))}
+        {/* Display the current answer if it exists */}
         {currentAnswer && <Message isQuestion={false}>{currentAnswer}</Message>}
       </ConversationContainer>
+      {/* Show a loading indicator when isLoading is true */}
       {isLoading && <p>Chargement...</p>}
 
       <InputContainer>
         <Input 
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
+          onChange={(e) => setInput(e.target.value)} // Update the input state on change
+          value={input} // Control the input with React state
           type="text"
         />
         <button onClick={handleSubmit}>Submit</button>
+        {isLoading && <button onClick={() => controller.abort()}>Cancel</button> }
       </InputContainer>
     </Container>
   );
 }
 
+// Styled-components for styling the app's components
 const Container = styled.div`
   padding: 0px 24px;
 `
